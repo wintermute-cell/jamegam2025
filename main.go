9d7e8504a2be0e83319658df00c7c77324207e36
wintermute <kjulius@gmx.de>
2025-01-09 23:14:50 +0100

    add web dist

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	gopher *ebiten.Image
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.Black)
+	ebitenutil.DebugPrint(screen, "Bye, World!")
+	screen.DrawImage(g.gopher, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -23,8 +28,16 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("Bye, World!")
+	game := &Game{}
+
+	var err error
+	game.gopher, _, err = ebitenutil.NewImageFromFile("gopher.png")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
