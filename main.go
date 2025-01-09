92db5303f877e14314c84f23e277cebbb83948fc
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2025-01-10 00:13:22 +0100

    movement test

--- main.go
+++ main.go
@@ -9,17 +9,39 @@ import (
 )
 
 type Game struct {
-	gopher *ebiten.Image
+	gopher       *ebiten.Image
+	gopherCursed *ebiten.Image
+	goperOptions *ebiten.DrawImageOptions
+	colorSet     bool
 }
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		g.goperOptions.GeoM.Translate(0, -1)
+	} else if ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.goperOptions.GeoM.Translate(-1, 0)
+	} else if ebiten.IsKeyPressed(ebiten.KeyS) {
+		g.goperOptions.GeoM.Translate(0, 1)
+	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.goperOptions.GeoM.Translate(1, 0)
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		g.colorSet = true
+	} else {
+		g.colorSet = false
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.Black)
 	ebitenutil.DebugPrint(screen, "Bye, World!")
-	screen.DrawImage(g.gopher, nil)
+	screen.Fill(color.White)
+	if g.colorSet {
+		screen.DrawImage(g.gopherCursed, g.goperOptions)
+	} else {
+		screen.DrawImage(g.gopher, g.goperOptions)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -37,6 +59,15 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	game.gopherCursed, _, err = ebitenutil.NewImageFromFile("MyGopher.png")
+	if err != nil {
+		panic(err)
+	}
+
+	game.goperOptions = new(ebiten.DrawImageOptions)
+
+	game.colorSet = false
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
