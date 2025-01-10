11397be7b0a332ad850746cde8b5f975127a71d1
wintermute <kjulius@gmx.de>
2025-01-10 12:34:10 +0100

    game init func

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -15,7 +15,39 @@ type Game struct {
 
 // NewGame creates a new Game instance
 func NewGame() *Game {
-	return &Game{}
+	g := &Game{}
+	g.Init()
+	return g
+}
+
+// Init initializes the game.
+func (g *Game) Init() {
+
+}
+
+// Update is part of the ebiten.Game interface.
+func (g *Game) Update() error {
+	specialUpdate(g)
+	for _, entity := range g.entities {
+		if err := entity.Update(g); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Draw is part of the ebiten.Game interface.
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.Black)
+	ebitenutil.DebugPrint(screen, "Bye, World!")
+	for _, entity := range g.entities {
+		entity.Draw(screen)
+	}
+}
+
+// Layout is part of the ebiten.Game interface.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
 }
 
 // AddEntity adds an entity to the game
@@ -45,29 +77,4 @@ func specialUpdate(g *Game) error {
 		}
 	}
 	return nil
-}
-
-// Update is part of the ebiten.Game interface.
-func (g *Game) Update() error {
-	specialUpdate(g)
-	for _, entity := range g.entities {
-		if err := entity.Update(g); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// Draw is part of the ebiten.Game interface.
-func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.Black)
-	ebitenutil.DebugPrint(screen, "Bye, World!")
-	for _, entity := range g.entities {
-		entity.Draw(screen)
-	}
-}
-
-// Layout is part of the ebiten.Game interface.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
 }
